ce655137dbc22843a3ed9104299b734459b14741
Kave <xnir.88@gmail.com>
2025-02-07 20:57:16 +0330

    Create app and add start handler

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import asyncio
-import telegram
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 from dotenv import load_dotenv
 import logging
 
 
 load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-
-TOKEN = os.getenv('TOKEN')
-
-async def main():
-    bot = telegram.Bot(TOKEN)
-    async with bot:
-        print(await bot.get_me())
-
+# write a handler for when the bot is started : /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I am a Bot!")
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token(TOKEN).build()
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    # run the app until ctrl+c is pressed
+    application.run_polling()
