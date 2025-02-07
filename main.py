95cd51cfab5733873d93457ee0801a0a8b2eb34c
Kave <xnir.88@gmail.com>
2025-02-07 21:03:50 +0330

    Add simple echo functionality to the bot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, filters, MessageHandler
 import os
 from dotenv import load_dotenv
 import logging
@@ -17,9 +17,15 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I am a Bot!")
 
+# write a handler to echo user input
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     application.add_handler(start_handler)
+    application.add_handler(echo_handler)
     # run the app until ctrl+c is pressed
     application.run_polling()
