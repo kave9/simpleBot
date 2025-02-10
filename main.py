ebf73974bd477380d60f7118793d5bcbf3cd30e8
Kave <xnir.88@gmail.com>
2025-02-10 12:54:12 +0330

    Add param names when passing args

--- main.py
+++ main.py
@@ -21,11 +21,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler(command= 'start', callback= start)
+    caps_handler = CommandHandler(command= 'caps', callback= caps)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+
     # run the app until ctrl+c is pressed
     application.run_polling()
